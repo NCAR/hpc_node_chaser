2a93a537757eaf86d827af1f40b0e09282756795
Davide Del Vento <davide.del.vento@gmail>
2013-04-30 08:39:53 -0600

    different names for the nodes

--- test_bparse.py
+++ test_bparse.py
@@ -3,22 +3,22 @@ import cStringIO
 
 def test_get_nodes_in_stringIO():
     s=cStringIO.StringIO("""
-Sat Apr 20 03:48:51 2013: Dispatched to 16384 Hosts/Processors <16*ys3769-ib> <
-                          16*ys3770-ib> <16*ys3771-ib> <16*ys3772-ib> <16*ys380
-                          1-ib> <16*ys3802-ib> <16*ys3803-ib> <16*ys3804-ib> <1
-                          6*ys3805-ib> <16*ys3806-ib> <16*ys3807-ib> <16*ys3808
-                          -ib> <16*ys3809-ib> <16*ys3810-ib> <16*ys3811-ib> <16
-                          *ys3812-ib> <16*ys3813-ib> <16*ys3814-ib> <16*ys3815-
-                          ib> <16*ys3816-ib> <16*ys3817-ib> <16*ys3818-ib> <16*
-                          ys3819-ib> <16*ys3820-ib> <16*ys3821-ib> 
-Fri Apr 19 02:07:32 2013: Starting (Pid 22545);
-                          By chance mention of <16*ys6352-ib>
+Sat Apr 20 03:48:51 2012: Dispatched to 16384 Hosts/Processors <16*be3769-ib> <
+                          16*be3770-ib> <16*be3771-ib> <16*be3772-ib> <16*be380
+                          1-ib> <16*be3802-ib> <16*be3803-ib> <16*be3804-ib> <1
+                          6*be3805-ib> <16*be3806-ib> <16*be3807-ib> <16*be3808
+                          -ib> <16*be3809-ib> <16*be3810-ib> <16*be3811-ib> <16
+                          *be3812-ib> <16*be3813-ib> <16*be3814-ib> <16*be3815-
+                          ib> <16*be3816-ib> <16*be3817-ib> <16*be3818-ib> <16*
+                          be3819-ib> <16*be3820-ib> <16*be3821-ib> 
+Fri Apr 19 02:07:32 2012: Starting (Pid 22545);
+                          By chance mention of <16*be6352-ib>
     """)
-    # note that ys6352-ib is mentioned by chance and should not be in the expected list
-    expected = ["ys3769-ib", "ys3770-ib", "ys3771-ib", "ys3772-ib", "ys3801-ib", 
-    "ys3802-ib", "ys3803-ib", "ys3804-ib", "ys3805-ib", "ys3806-ib", "ys3807-ib", 
-    "ys3808-ib", "ys3809-ib", "ys3810-ib", "ys3811-ib", "ys3812-ib", "ys3813-ib",
-    "ys3814-ib", "ys3815-ib", "ys3816-ib", "ys3817-ib", "ys3818-ib", "ys3819-ib",
-    "ys3820-ib", "ys3821-ib"]
+    # note that be6352-ib is mentioned by chance and should not be in the expected list
+    expected = ["be3769-ib", "be3770-ib", "be3771-ib", "be3772-ib", "be3801-ib", 
+    "be3802-ib", "be3803-ib", "be3804-ib", "be3805-ib", "be3806-ib", "be3807-ib", 
+    "be3808-ib", "be3809-ib", "be3810-ib", "be3811-ib", "be3812-ib", "be3813-ib",
+    "be3814-ib", "be3815-ib", "be3816-ib", "be3817-ib", "be3818-ib", "be3819-ib",
+    "be3820-ib", "be3821-ib"]
     nodes=bparse._get_nodes_in_stringIO(s)
     assert nodes == expected
