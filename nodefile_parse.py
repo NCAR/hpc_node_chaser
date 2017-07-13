f13354be98424ab068169a5b7f4ff23c204ec3f8
Davide Del Vento <davide.del.vento@gmail>
2017-07-13 09:32:21 -0600

    stub code for parsing files

---
+++ nodefile_parse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+# from __future__ import with_statement
+import helper as h
+
+def parse(data):
+    return "a"
+
+def get_nodes_in_file(filename):
+    h.log("\n---------------------------------------\nProcessing file " + str(filename))
+    try:
+        with open(filename) as f:
+            nodes = parse(f)
+    except IOError as ioe:
+        print ioe
+        sys.exit(1)
+    h.log("Ended processing file " + str(filename) + ", " + str(len(nodes)) + " nodes found.")
+    return nodes
+
+if __name__ == '__main__':
+    h.run(help_msg = "Find bad performing nodes from a list of good and bad files",
+          get_nodes_in_job = get_nodes_in_file, expected_type=str)
+
